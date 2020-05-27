7810105043a4be88f191f0e95b97e890f40f1934
yeait2 <54796454+yeait2@users.noreply.github.com>
2020-05-27 16:24:48 +0900

    Update adt7410_2.py

--- adt7410_2.py
+++ adt7410_2.py
@@ -7,7 +7,7 @@ address = 0x48
 index = 0
 while index < 10:
     block = i2c.read_i2c_block_data(address, 0x00, 12)
-    temp -= 8192
+    temp = (block[0] << 8 | block[1]) >> 3
     
     if(temp >= 4096):
         temp -= 8192
